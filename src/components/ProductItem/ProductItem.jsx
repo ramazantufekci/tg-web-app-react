d1ee90076559150467033ad8999e4b8178bdb985
ramazantufekci <ramazan.tufekci@outlook.com>
2024-07-16 12:25:44 +0000

    yanlışları düzelttim product

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,10 +9,10 @@ const ProductItem = ({product, className, onAdd}) =>{
     return (
         <div className={'product '+className}>
             <div className={'img'}/>
-            <div className={'title'}>{ProductItem.title}</div>
-            <div className={'description'}>{ProductItem.description}</div>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Fiyat: <b>{ProductItem.price}</b></span>
+                <span>Fiyat: <b>{product.price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>Sepete Ekle</Button>
 
